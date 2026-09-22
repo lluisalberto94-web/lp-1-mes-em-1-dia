44324e30b858040e9d92465638d0773ae0d4dc2a
lluisalberto94-web <l.luisalberto94@gmail.com>
2026-09-22 09:39:30 -0300

    fix(content-os): force root alias in webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
+import path from 'node:path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { serverActions: { bodySizeLimit: '2mb' } },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(process.cwd());
+    return config;
+  },
 };
+
 export default nextConfig;
